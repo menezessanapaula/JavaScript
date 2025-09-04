68c8c675dee1933416e60374cd64759fd6566147
Ana Paula Menezes <ap.souzamenezes@gmail.com>
2025-09-04 06:46:05 -0300

    Aula 10 e 11

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 68.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel >= 60) { //condição simples
+    console.log(`Você está sendo multado por excesso de velocidade!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ Aula 11/ex009.js
@@ -0,0 +1,6 @@
+var país = "França"
+if (país == "Brasil") {
+    console.log("Você é Brasileiro!")
+} else {
+    console.log("Você é Estrangeiro!")
+}
